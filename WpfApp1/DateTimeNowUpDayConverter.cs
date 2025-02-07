7d5fc4f4bab47d05dcd32cbe341d85151dc535db
Student <Student@PC-09>
2025-02-07 15:09:24 +0500

    a

---
+++ WpfApp1/DateTimeNowUpDayConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApp1
+{
+    class DateTimeNowUpDayConverter : IValueConverter
+    {
+        public object Convert(object value,
+                Type targetType, object parameter, CultureInfo culture)
+        {
+            var d = value as DateTime?;
+            if (d != null)
+            {
+                return DateTime.Now.Date.AddDays(1) == d.Value;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value,
+            Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -100,25 +100,5 @@ namespace WpfApp1
                 EndToDo();
             }
         }
-        public class DateTimeLessDateConverter : IValueConverter
-        {
-            public object Convert(object value,
-                Type targetType, object parameter, CultureInfo culture)
-            {
-                var d = value as DateTime?;
-                if(d != null )
-                {
-                    return DateTime.Now > d.Value;
-                }
-
-                return false;
-            }
-
-            public object ConvertBack(object value,
-                Type targetType, object parameter, CultureInfo culture)
-            {
-                throw new NotImplementedException();
-            }
-        }
     }
 }
